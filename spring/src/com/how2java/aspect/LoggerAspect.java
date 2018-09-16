356edddcb15ecf8bfd694a26091a7be0a48370b7
chenkeng <chenchaohan@outlook.com>
2018-09-16 18:34:44 +0800

    注解方式AOP

--- spring/src/com/how2java/aspect/LoggerAspect.java
+++ spring/src/com/how2java/aspect/LoggerAspect.java
@@ -1,5 +1,8 @@
 package com.how2java.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 
 /**
@@ -8,7 +11,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
  * @Date: 2018/9/16 17:06
  * @Description:
  */
+
+@Aspect
+@Component
 public class LoggerAspect {
+
+    @Around(value = "execution(* com.how2java.service.ProductService.*(..))")
     public Object log(ProceedingJoinPoint joinPoint) throws Throwable{
         System.out.println("start log:" + joinPoint.getSignature().getName());
         Object object=joinPoint.proceed();
--- spring/src/com/how2java/service/ProductService.java
+++ spring/src/com/how2java/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.how2java.service;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @Auther: superhan
  * @Contact: chenchaohan@live.com
  * @Date: 2018/9/16 17:02
  * @Description:
  */
+@Component("s")
 public class ProductService {
     public void doSomeService(){
         System.out.println("doSomeService");
